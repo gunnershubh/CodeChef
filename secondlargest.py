add0979a84339a3210a06cc0303e3b1ca61be32b
Shubham Shekhar <shubham@Shubhams-MacBook-Air.local>
2021-08-03 09:10:09 +0530

    Code Chef Solution for Second Largest Number

---
+++ secondlargest.py
@@ -0,0 +1,18 @@
+'''
+Three numbers A, B and C are the inputs. Write a program to find second largest among them.
+'''
+
+a = 112
+b = 456
+c = 872
+
+list1 = []
+
+list1.extend([a,b,c])
+
+a = max(list1)
+
+print(a)
+
+
+
